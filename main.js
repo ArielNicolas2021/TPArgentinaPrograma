bc044f9d8b0b453bcdd2e166c1050a34df0c557b
ArielNicolas2021 <89411353+ArielNicolas2021@users.noreply.github.com>
2023-04-10 16:30:58 -0300

    pagina basada en autocity.com.ar

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-window.addEventListener("load", (event) => {
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".menu-open");
+const closeMenuBtn = document.querySelector(".menu-close");
 
+function toggleMenu() {
+    menu.classList.toggle("menu-opened");
+}
 
-
-});
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
