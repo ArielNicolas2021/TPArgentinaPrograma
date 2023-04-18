61fa50d554d752b0518965131bec167d477f299a
ArielNicolas2021 <89411353+ArielNicolas2021@users.noreply.github.com>
2023-04-18 11:24:31 -0300

    Add files via upload

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,35 +1,14 @@
-const menu = document.querySelector(".menu");
-const openMenuBtn = document.querySelector(".menu-open");
-const closeMenuBtn = document.querySelector(".menu-close");
-
-function toggleMenu() {
-    menu.classList.toggle("menu-opened");
-}
-
-openMenuBtn.addEventListener("click", toggleMenu);
-closeMenuBtn.addEventListener("click", toggleMenu);
-
-var nuevos = document.getElementById('nuevos-btn');
-var usados = document.getElementById('usados-btn');
-var checkbox = document.querySelectorAll('.checkbox');
-var nuevosContent = document.getElementById ('nuevos');
-var usadosContent = document.getElementById ('usados');
-
-
-nuevos.addEventListener('click',function(){
-    nuevosContent.style.display = 'none';
-    checkbox.forEach((e) => {
-        if(e.checked == true){
-            nuevosContent.style.display = 'block';
+$(function(){
+    $("#nuevos-check").on("click",function () {
+        if ($("#nuevos-check:checked")) {
+            $("#nuevos").show();
+            $("#usados").hide();
         }
     });
-});
-usados.addEventListener('click',function(){
-    usadosContent.style.display = 'none';
-    nuevosContent.style.display = 'none';
-    checkbox.forEach((e)=>{
-        if(e.checked==true){
-            nuevosContent.style.display = 'block';
+    $("#usados-check").on("click",function () {
+        if ($("#usados-check:checked")) {
+            $("#usados").show();
+            $("#nuevos").hide();
         }
     });
 });
---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".menu-open").click(function(){
+        $(".menu").toggleClass("menu-opened");
+    });
+    $(".menu-close").click(function(){
+        $(".menu").toggleClass("menu-opened");
+    })
+});
