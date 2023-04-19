cb649d2ab37798814f2e25d05b3da507d6be922a
ArielNicolas2021 <89411353+ArielNicolas2021@users.noreply.github.com>
2023-04-19 18:10:34 -0300

    Add files via upload

--- js/sobreNosotros.js
+++ js/sobreNosotros.js
@@ -1,10 +1,11 @@
-const menu = document.querySelector(".menu");
-const openMenuBtn = document.querySelector(".menu-open");
-const closeMenuBtn = document.querySelector(".menu-close");
-
-function toggleMenu() {
-    menu.classList.toggle("menu-opened");
+function iniciarMap(){
+    var coord = {lat:-34.6187127 ,lng: -58.3691909};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
 }
-
-openMenuBtn.addEventListener("click", toggleMenu);
-closeMenuBtn.addEventListener("click", toggleMenu);
