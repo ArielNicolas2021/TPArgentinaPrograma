dde145795a9609b8cccfd3499ab65e5d81fd6703
lHuevol <108162184+lHuevol@users.noreply.github.com>
2023-04-19 23:14:01 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -42,7 +42,7 @@ submitBtn.addEventListener("click", function(){
   current += 1;
   setTimeout(function(){
     alert("Your Form Successfully Signed up");
-    location.reload();
+    
   },800);
 });
 
